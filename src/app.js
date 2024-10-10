6fa99a76691831a7860de04268904166b3aa6ce7
antomanu95 <antoniomr.estevez@hotmail.com>
2024-10-10 12:59:07 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,23 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  console.log("Hello Rigo from the console");
 };
+
+let who = ["The dog", "My grandma", "The mailman", "My bird"];
+let action = ["ate", "peed", "crushed", "broke"];
+let what = ["my homework", "my phone", "the car"];
+let when = [
+  "before the class",
+  "when I was sleeping",
+  "while I was exercising",
+  "during my lunch",
+  "while I was praying"
+];
+
+const generateRandom = arr => Math.floor(Math.random() * arr.length);
+const getValue = arr => arr[generateRandom(arr)];
+console.log(getValue(who));
+console.log(getValue(action));
+console.log(getValue(what));
+console.log(getValue(when));
